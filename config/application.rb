efb76bef74ccdaf836bca3ce66797ea494dc8af9
Kurt Landrus <klandrus@gmail.com>
2020-05-28 14:02:13 -0400

    complete chap 3

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Projects
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # DEPRECATION WARNING: Leaving `ActiveRecord::ConnectionAdapters::SQLite3Adapter.represent_boolean_as_integer`
+    # set to false is deprecated.
+    Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
